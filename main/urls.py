11cdd770f6d17b337b78b50f4a18b7799a45e62a
elenazahrak <elenazahraa9@gmail.com>
2023-11-15 16:14:23 +0700

    Tutorial 8 selesai (Django)

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from main.views import show_main
 from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml 
 from main.views import show_main, create_product, show_xml, show_json
@@ -11,6 +10,7 @@ from main.views import edit_product
 from main.views import delete_product
 from main.views import get_product_json
 from main.views import add_product_ajax
+from main.views import create_product_flutter, show_main
 
 app_name = 'main'
 
@@ -27,6 +27,7 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
 
